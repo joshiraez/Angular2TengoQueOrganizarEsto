8754f5ec7230320b934b2cfb2f97d984394273f5
joseraez <joseraezrodriguez@gmail.com>
2017-03-28 15:37:42 +0200

    first commit

---
+++ ownersControllers.js
@@ -0,0 +1,25 @@
+(function () {
+var modulo = angular.module("petclinicApp");
+modulo.controller('OwnersControllers', function(ownersFactory) {
+
+   var vm = this;
+
+    vm.name;
+    vm.address;
+    vm.phone;
+    vm.numPets;
+    
+   vm.filtro = "P";
+    
+    vm.owners = ownersFactory.getOwners();
+    vm.deleteOwner = function(index) {
+        ownersFactory.deleteOwner(index);
+    };
+    
+    vm.saveOwner = function(){
+        ownersFactory.createOwner(vm.name, vm.address, vm.phone, vm.numPets);
+    };
+    
+   }
+  );
+}());
---
+++ ownersFactory.js
@@ -0,0 +1,25 @@
+(function () {
+var modulo = angular.module("petclinicApp");
+modulo.factory('ownersFactory', ownersFactory);
+                  
+   function ownersFactory () {
+      var owners = [{'name':'Propietario 1', 'address':'Cartuja',                      'phone':'654789654','numPets':2},
+                           {'name':'Propietario 2', 'address':'Cartuja', 'phone':'654789654','numPets':0},
+                           {'name':'Propietario 3', 'address':'Cartuja', 'phone':'654789654','numPets':1}];
+
+   
+   var interface = {
+           getOwners : function(){
+               return owners;
+           },
+           createOwner : function (name, address, phone, numPets){
+               owners.push({'name':name,'address':address,'phone':phone,'numPets':numPets});
+           },
+             deleteOwner : function (index) {
+                owners.splice(index, 1);
+
+            }
+        }
+    return interface;
+    }
+} () );
